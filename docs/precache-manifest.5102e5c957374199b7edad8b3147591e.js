bc9fc8de63e2b5f6b3506541a378d611250928c6
cameron-poehner <cameronpoehner@gmail.com>
2020-10-22 11:26:53 -0700

    attempted new method for optimized app build

---
+++ docs/precache-manifest.5102e5c957374199b7edad8b3147591e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20b27dd0a78c8193e9980b87014e7a7b",
+    "url": "/amdb/index.html"
+  },
+  {
+    "revision": "273670b5e3d212d95f27",
+    "url": "/amdb/static/css/main.5e632dd4.chunk.css"
+  },
+  {
+    "revision": "bb1bc39db4132a81c17d",
+    "url": "/amdb/static/js/2.27e69889.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/amdb/static/js/2.27e69889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "273670b5e3d212d95f27",
+    "url": "/amdb/static/js/main.4fa12037.chunk.js"
+  },
+  {
+    "revision": "df84426ed3cdaf08541a",
+    "url": "/amdb/static/js/runtime-main.83360176.js"
+  }
+]);
